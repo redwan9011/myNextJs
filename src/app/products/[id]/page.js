9204166c55b4ad9852956e65d6e60e2934e879b9
redwan9011 <redwan390309@gmail.com>
2024-03-26 23:20:14 +0600

    practicing next js

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -6,6 +6,11 @@ const dynamicpage = ({params, searchParams}) => {
     return (
         <div>
             <h1>this is dynamic page {params.id} {searchParams.category} </h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet ipsa hic qui iure, nostrum debitis tempore numquam magnam illo nesciunt recusandae illum voluptatem veniam error voluptate veritatis quasi. Eius, libero.</p>
+            <div> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea maiores aspernatur saepe corporis, possimus reprehenderit iste voluptate voluptatibus doloribus corrupti.
+
+            </div>
+             
         </div>
     );
 };
