1872a615792bc624af7e172fe088f9081b3f8604
redwan9011 <redwan390309@gmail.com>
2024-04-26 22:48:32 +0600

    add increase and decrease button

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
-import Image from "next/image";
+"use client"
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Home() {
+  const [counter, setCounter] = useState(0)
   return (
     <div>
       <Link  href="/contact"> contact</Link>
+      <h2>Counter: {counter}</h2>
+     <button className="btn btn-primary " onClick={()=> setCounter(counter +1)}>Increase</button>
+     <button className="btn btn-primary" onClick={()=> setCounter(counter-1)}>Decrease</button>
     </div>
  
   );
