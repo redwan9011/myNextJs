982f468e010f7b067e40005e5e123b8425fb581d
redwan9011 <redwan390309@gmail.com>
2024-04-27 16:19:02 +0600

    add sohan page

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,12 @@
 "use client"
 
+import { useState } from "react";
+
 const AboutUs = () => {
+    const [count, setCount] = useState()
     return (
         <div>
-            
+            <h1>THis is about us</h1>
         </div>
     );
 };
---
+++ src/app/sohan/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const sohan = () => {
+    return (
+        <div>
+            this is sohan page
+        </div>
+    );
+};
+
+export default sohan;
